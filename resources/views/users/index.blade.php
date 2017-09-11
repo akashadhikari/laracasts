9872802e6178bf718b43b0b1ae5886e3b6416e52
Akash Adhikari <akash_532998@yahoo.com>
2017-09-11 17:25:04 +0545

    random

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,6 +3,19 @@
 @section('content')
   
 
-        Users!
+        <div class="col-sm-8 blog-main">
+
+          Users
+
+          
+          <nav class="blog-pagination">
+
+            <a class="btn btn-outline-primary" href="#">Older</a>
+
+            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+
+          </nav>
+
+        </div><!-- /.blog-main -->
       
 @endsection
